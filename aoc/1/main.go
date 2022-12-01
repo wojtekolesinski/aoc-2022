a4c843864fd50a5757ce53888d4238420147214c
wojtekolesinski <wojtek.olesinski00@gmail.com>
2022-12-01 19:54:54 +0100

    day 1

---
+++ aoc/1/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"log"
+)
+
+func checkError(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func main() {
+
+	file, err := os.Open("aoc/1/input.txt")
+	checkError(err)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var input []string
+
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	var sums []int
+	var sum int = 0
+
+	for _, el := range input {
+		if strings.TrimSpace(el) == "" {
+			sums = append(sums, sum)
+			sum = 0
+		} else {
+			intVal, err := strconv.Atoi(el)
+			checkError(err)
+			sum += intVal
+		}
+	}
+
+	max := findTopNSum(sums, 3)
+
+	fmt.Println(max)
+}
+
+// part 1
+func findMax(sums []int) int {
+	max := 0
+	for _, el := range sums {
+		if el > max {
+			max = el
+		}
+	}
+	return max
+}
+
+// part 2
+func findTopNSum(sums []int, n int) int {
+	topN := make([]int, n)
+
+	for _, el := range sums {
+		if el < topN[0] {
+			continue
+		}
+		for i := n - 1; i >= 0; i-- {
+			if el > topN[i] {
+				for j := 0; j < i; j++ {
+					topN[j] = topN[j+1]
+				}
+				topN[i] = el
+				break
+			}
+		}
+	}
+
+	sum := 0
+	for _, el := range topN {
+		sum += el
+	}
+	return sum
+}
