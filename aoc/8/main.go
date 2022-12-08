7bc5e93dfe7c53d433fa9d7a8c504c631e292d5c
wojtekolesinski <wojtek.olesinski00@gmail.com>
2022-12-08 16:02:45 +0100

    day 8 part 2

--- aoc/8/main.go
+++ aoc/8/main.go
@@ -61,9 +61,48 @@ func isVisible(grid [][]int, row, col int) bool {
 	return visible
 }
 
+func getScenicScore(grid [][]int, row, col int) int {
+	el := grid[row][col]
+	// score from top
+	top := 0
+	for i := row-1; i >= 0; i-- {
+		top++
+		if grid[i][col] >= el {
+			break
+		}
+	}
+
+	// score from bottom
+	bottom := 0
+	for i := row+1; i < len(grid); i++ {
+		bottom++
+		if grid[i][col] >= el {
+			break
+		}
+	}
+
+	// score from right
+	right := 0
+	for i := col+1; i < len(grid[0]); i++ {
+		right++
+		if grid[row][i] >= el {
+			break
+		}
+	}
+
+	// score from left
+	left := 0
+	for i := col-1; i >= 0; i-- {
+		left++
+		if grid[row][i] >= el {
+			break
+		}
+	}
+	return top * bottom * left * right
+}
+
 func part1() {
 	lines := s.Split(input, "\n")
-	// const nrows, ncols int = 5, 5
 
 	grid := make([][]int, len(lines))
 	for i := range grid {
@@ -88,6 +127,34 @@ func part1() {
 	fmt.Println(sum)
 }
 
+func part2() {
+	lines := s.Split(input, "\n")
+
+	grid := make([][]int, len(lines))
+	for i := range grid {
+		grid[i] = make([]int, len(lines[0]))
+	}
+	for row, line := range lines {
+		for column, tree := range line {
+			grid[row][column] = int(tree - '0')
+		}
+	}
+
+	maxScore := 0
+	for i := 1; i < len(grid)-1; i++ {
+		for j := 1; j < len(grid)-1; j++ {
+			score := getScenicScore(grid, i, j)
+			if score > maxScore {
+				maxScore = score
+			}
+		}
+	}
+
+	fmt.Println(grid)
+	fmt.Println(maxScore)
+}
+
 func main() {
-	part1()
+	// part1()
+	part2()
 }
